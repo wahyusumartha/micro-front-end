f7de301c7fc0d575b252ee4fae3889fd329e49e8
Wahyu Sumartha <wahyusumartha@gmail.com>
2020-12-28 16:52:32 +0100

    Protecting dashboard from unauthenticated session

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,14 @@
+import { mount } from 'dashboard/DashboardApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default () => {
+    const ref = useRef(null);
+    const history = useHistory(); 
+
+    useEffect(() => {
+        mount(ref.current);
+    }, []);
+
+    return <div ref={ref} />
+};
